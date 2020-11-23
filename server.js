293c8d5e2a6bf3561eb01132dc62da68d67c5136
jeececab <jcseguincabana@gmail.com>
2020-11-22 22:37:33 -0500

    set

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ const sessionConfig = {
   saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-    httpOnly: true,
+    //httpOnly: true,
     sameSite: 'none',
-    secure: process.env.ENV === 'production',
-    domain: 'shielded-hamlet-36885.herokuapp.com'
+    //secure: process.env.ENV === 'production',
+    domain: '.shielded-hamlet-36885.herokuapp.com'
   }
 };
 
