3f623f23c8c94bd79660c65a2b5e474a4864f44a
jeececab <jcseguincabana@gmail.com>
2020-11-22 20:32:20 -0500

    added pwa prod url

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.set('trust proxy', 1);
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.ENV === 'production' ? 'https://web-journal.netlify.app' : 'http://localhost:3000',
     credentials: true
   })
 );
